3d8c189df60b5f8e90d05e8e66938f8550e0a50f
temilaj <temilaj@hotmail.com>
2022-01-20 01:52:54 -0500

    ft(layout): add icons

---
+++ components/icons/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home(props) {
+  const { isActive = false } = props;
+  return (
+    <svg width={46} height={50} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M40.475 45h-7.474V27.5a2.5 2.5 0 0 0-2.5-2.5h-15a2.498 2.498 0 0 0-2.5 2.5V45H5.5l.014-21.043L22.995 6.08l17.506 17.98L40.474 45ZM18 45h10V30H18v15Zm26.06-24.538L24.788.752c-.942-.962-2.633-.962-3.575 0L1.938 20.466C1.025 21.403.5 22.712.5 24.06V45c0 2.758 2.118 5 4.72 5h35.558c2.602 0 4.723-2.242 4.723-5V24.06c0-1.348-.526-2.657-1.44-3.598Z"
+        fill="#757D8A"
+      />
+    </svg>
+  );
+}
---
+++ components/icons/LogOut.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function LogOut(props) {
+  const { isActive = false } = props;
+  return (
+    <svg width={43} height={40} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10 2.5C10 3.875 8.875 5 7.5 5H5v30h2.5c1.375 0 2.5 1.125 2.5 2.5S8.875 40 7.5 40h-5A2.507 2.507 0 0 1 0 37.5v-35C0 1.125 1.125 0 2.5 0h5C8.875 0 10 1.125 10 2.5Zm25.01 6.062 7.035 10A2.495 2.495 0 0 1 42 21.5l-7.5 10a2.495 2.495 0 0 1-3.5.5 2.498 2.498 0 0 1-.5-3.498l4.502-6.002H15a2.5 2.5 0 1 1 0-5h20c.041 0 .08.01.118.022.031.01.063.02.097.023l-4.295-6.108a2.499 2.499 0 0 1 .607-3.482 2.496 2.496 0 0 1 3.483.607Z"
+        fill="#757D8A"
+      />
+    </svg>
+  );
+}
